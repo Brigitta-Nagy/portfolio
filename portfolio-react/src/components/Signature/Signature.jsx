438e470d6324b3fcd2b5c25681909d5b90d991ef
Brigitta Nagy <brigitta.nagy@bgreat.co.uk>
2022-12-27 20:24:07 +0000

    main page icons

--- portfolio-react/src/components/Signature/Signature.jsx
+++ portfolio-react/src/components/Signature/Signature.jsx
@@ -40,34 +40,36 @@ function Signature() {
           initial={{opacity:0}} 
           animate={{opacity:1}}
           transition={{ ease: "easeOut", duration: 1, delay:3.5 }} src={HTML} alt="" />
+       
         <m.img 
           initial={{opacity:0}} 
           animate={{opacity:1}}
-          transition={{ ease: "easeOut", duration: 2, delay:4 }} src={CSS} alt="" />
+          transition={{ ease: "easeOut", duration: 2, delay:4 }} src={Sass_logo} alt="" />
         <m.img 
           initial={{opacity:0}} 
           animate={{opacity:1}}
-          transition={{ ease: "easeOut", duration: 2, delay:4.5 }} src={Sass_logo} alt="" />
+          transition={{ ease: "easeOut", duration: 2, delay:4.5}} src={Express_logo} alt="" />
         <m.img 
           initial={{opacity:0}} 
           animate={{opacity:1}}
           transition={{ ease: "easeOut", duration: 2, delay:5}} src={Bootstrap_logo} alt="" />
-        <m.img 
-          initial={{opacity:0}} 
-          animate={{opacity:1}}
-          transition={{ ease: "easeOut", duration: 2, delay:5.5 }} src={Express_logo} alt="" />
+      
         <m.img 
           initial={{opacity:0}} 
           animate={{opacity:1, rotate: 360}}
-          transition={{ ease: "easeOut", duration: 2, delay:6 }} src={NodeJs} alt="" />
-        <m.img  className={styles.mongodbLogo}
+          transition={{ ease: "easeOut", duration: 2, delay:5.5 }} src={NodeJs} alt="" />
+        <m.img 
+          initial={{opacity:0}} 
+          animate={{opacity:1}}
+          transition={{ ease: "easeOut", duration: 2, delay:6 }} src={CSS} alt="" />
+        <m.img  
           initial={{opacity:0}} 
           animate={{opacity:1}}
           transition={{ ease: "easeOut", duration: 2, delay: 6.5 }}src={Mongodb_logo} alt="" />
         <m.img 
         initial={{x:1200}} 
         animate={{x:0, rotate: 360}}
-        transition={{ ease: "easeOut", duration: 2, delay:5 }} src={JQuery_logo} alt="" />
+        transition={{ ease: "easeOut", duration: 2, delay:5.5 }} src={JQuery_logo} alt="" />
       </div>
       </div>
     </div>
