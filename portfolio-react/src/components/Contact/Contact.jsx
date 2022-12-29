4e557f46249247847a1da777cdce37dab81878b4
Brigitta Nagy <brigitta.nagy@bgreat.co.uk>
2022-12-29 09:35:32 +0000

    contact form

--- portfolio-react/src/components/Contact/Contact.jsx
+++ portfolio-react/src/components/Contact/Contact.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import styles from './Contact.module.css'
 import place from '../../static/place.png'
 import email from '../../static/email.png'
+import { useRef } from 'react'
 
 function Contact() {
+  const formRef = useRef()
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
   return (
     <section className={styles.contactContainer} id='contact'>
         <div className={styles.contactBackground}></div>
@@ -23,10 +29,11 @@ function Contact() {
           <div className={styles.contactRight}>
             <p className={styles.description}>
               <h3>Hello! Do you have project to complete? Let's discuss about it! </h3>
-              Always freelancing if the right project finds me. </p>
-            <form>
+              Always available for freelancing if the right project finds me. </p>
+            <form ref={formRef} onSubmit={handleSubmit}>
               <input type='text' placeholder='Name' name='user_name'/>
               <input type='text' placeholder='Email' name='user_email'/>
+              <input type='text' placeholder='Subject' name='user_subject'/>
               <textarea rows='5' placeholder='Message' name='user_message'/>
               <button>Submit</button>
             </form>
