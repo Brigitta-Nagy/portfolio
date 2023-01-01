37d3b5326b79ef98b3a9fd955eae14d14bcc864c
Brigitta Nagy <brigitta.nagy@bgreat.co.uk>
2023-01-01 16:43:51 +0000

    more projects

--- portfolio-react/src/components/Projects/Projects.jsx
+++ portfolio-react/src/components/Projects/Projects.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Weather from '../../static/weather-app.png'
+
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import styles from './Projects.module.css';
@@ -24,8 +24,43 @@ function Projects() {
       </div>
     </div>
 
+    <div className={styles.project_card} data-aos='zoom-in'>
+      <div className={`${styles.project_img} ${styles.project_img2}` }>
+       
+      </div>
+        <div className={styles.project_content_wrapper}>
+            <div>
+              <div className={styles.project_title}>
+              Weather application
+              <div className={styles.project_tech}>JS HTML CSS JQUERY</div>
+            </div>
+            <div className={styles.project_description}>
+              Front-end application allows you to get weather information in real time in your searched city. Temperature is displayed in Celsius or Farenheit as you wish by clicking the button. 
+              The background image  changes depends on the weather 
+            </div>
+          </div>
+      </div>
+    </div>
     <div className={styles.project_card} id='projects'>
-      <div className={styles.project_img}>
+      <div className={`${styles.project_img} ${styles.project_img3}` }>
+       
+      </div>
+        <div className={styles.project_content_wrapper}>
+            <div>
+              <div className={styles.project_title}>
+              Weather application
+              <div className={styles.project_tech}>JS HTML CSS JQUERY</div>
+            </div>
+            <div className={styles.project_description}>
+              Front-end application allows you to get weather information in real time in your searched city. Temperature is displayed in Celsius or Farenheit as you wish by clicking the button. 
+              The background image  changes depends on the weather 
+            </div>
+          </div>
+      </div>
+    </div>
+    <div className={styles.project_card} id='projects'>
+      <div className={`${styles.project_img} ${styles.project_img4}` }>
+       
       </div>
         <div className={styles.project_content_wrapper}>
             <div>
