1318926a8bb70eecff8aabf1251aa746df54f778
Brigitta Nagy <brigitta.nagy@bgreat.co.uk>
2023-02-02 15:16:42 +0000

    img slider

---
+++ portfolio-react/src/components/Signature/images.jsx
@@ -0,0 +1,8 @@
+
+import images1  from "../../static/julien-brion.jpg"
+import images2  from "../../static/junior-teixeira.jpg"
+import images3  from "../../static/lisa-fotios.jpg"
+import images4  from "../../static/markus-spiske.jpg"
+import images5  from "../../static/cottonbro-studio.jpg"
+
+export default [images1, images2, images3, images4, images5]
